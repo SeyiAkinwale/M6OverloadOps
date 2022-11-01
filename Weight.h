37c08c9f9efc9afc3d9f96a314a468f98fbf9a94
Seyi <seyi.k.akinwale@gmail.com>
2022-11-01 10:22:31 -0700

    getting header file setup

--- Weight.h
+++ Weight.h
@@ -1,31 +1,16 @@
-//Specification for the Rational class  
+//Specification for the Weight class  
 
 #ifndef M6WEIGHT_H
 #define M6WEIGHT_H
 
-//Rational class declaration
-class Rational {
+//Weight class declaration
+class Weight {
 private:
-	int numerator;
-	int denominator = 1; //not undefined Rational value when object created
-	void setNonzeroDenom(int); //sets denominator to 1 if arg=0
-	void reduceFrac(int&, int&);
+
+
 public:
-	Rational();
-	Rational(int, int);
-	void setNumerator(int);
-	void setDenominator(int);
-	void setRational(int, int); //sets numerator & denominator
-	int getNumerator();
-	int getDenominator();
 
-	Rational add(const Rational&) const;
-	Rational subtract(const Rational&) const;
-	Rational multiply(const Rational&) const;
-	Rational divide(const Rational&) const;
 
-	void printRatioForm();
-	void printFloatForm();
 };
 
 #endif
